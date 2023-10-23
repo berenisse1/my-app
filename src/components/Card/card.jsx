a4a6800d18f3c0180bf3d6196f9bcf2b802ae2cc
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-23 20:08:38 +0200

    card update

---
+++ src/components/Card/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import '../styles/Card.css'
+
+
+function Card({image, title}) {
+    return (
+      <div className="card">
+        <img src={image} alt={title} className="card-img" />
+        <h2 className="card-title">{title}</h2>
+      </div>
+    );
+}
+
+export default Card;
