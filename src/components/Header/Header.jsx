214a6b50e0b41430094ac69260c2ef52e809796a
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-23 20:05:43 +0200

    logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom"
+import logo from '../../assets/logo.png'
 
 function Header(){
     return(
         <header>
+            <div>
+                <img className="logo" src={logo} alt="logo agence kasa" />
+            </div>
             <nav>
                 <Link to="/">Acceuil</Link>
                 <Link to="/a-propos">A propos</Link>
