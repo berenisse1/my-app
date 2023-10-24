afab55115add539217988794d16826aaef455b13
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-24 06:51:40 +0200

    errorPage 404 fix

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import Error from './pages/ErrorPage/ErrorPage'
-//import ErrorPage from './pages/ErrorPage/ErrorPage'
- 
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+
 ReactDOM.render(
     <React.StrictMode>
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/a-propos" element={<About />} />
-                <Route path="*" element={<Error />} />
+                <Route path="*" element={<ErrorPage />} />
             </Routes>
                   </Router>
     </React.StrictMode>,
