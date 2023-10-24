c8a2471da2627f50ca26bc7782daa47d09cfc0ed
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-24 05:49:13 +0200

    ErrorPage route delete

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import ErrorPage from './pages/ErrorPage/ErrorPage'
+//import ErrorPage from './pages/ErrorPage/ErrorPage'
  
 ReactDOM.render(
     <React.StrictMode>
@@ -11,7 +11,6 @@ ReactDOM.render(
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/a-propos" element={<About />} />
-                <Route path="*" element={<ErrorPage />} />
             </Routes>
                   </Router>
     </React.StrictMode>,
