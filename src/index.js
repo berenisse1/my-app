690ae84ab9c46f0cb472b86e586196d0f4571d45
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-24 06:09:20 +0200

    route Error 404

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import Error from './pages/ErrorPage/ErrorPage'
 //import ErrorPage from './pages/ErrorPage/ErrorPage'
  
 ReactDOM.render(
@@ -11,6 +12,7 @@ ReactDOM.render(
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/a-propos" element={<About />} />
+                <Route path="*" element={<Error />} />
             </Routes>
                   </Router>
     </React.StrictMode>,
