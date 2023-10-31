dc1a41b8a138b811f0ea3cac20945054299432bc
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-31 05:32:23 +0100

    route page logement

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import ErrorPage from './pages/ErrorPage/ErrorPage'
+import Logement from "./pages/Logement/Logement";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.render(
                 <Route path="/" element={<Home />} />
                 <Route path="/a-propos" element={<About />} />
                 <Route path="*" element={<ErrorPage />} />
+                <Route path="/logements/id" element={<Logement/>} />
             </Routes>
                   </Router>
     </React.StrictMode>,
