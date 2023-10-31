7893f2b9fafe89cf1dae2455d508707eac9a698c
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-31 05:39:48 +0100

    logoFooter import

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '../../assets/logo.png'
+import logoFooter from '../../assets/logoFooter.png'
 import '../../styles/Footer.css'
 
 function Footer () {
     return (
       <footer className="footer">
-        <img src={logo} alt="logo agence kasa" className="footer-logo" />
+        <img src={logoFooter} alt="logo agence kasa" className="footer-logo" />
         <p className="footer-text">
           © 2020 Kasa. All rights reserved
         </p>
