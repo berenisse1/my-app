d40a432deb3ec46031276f874ac2b9684a9dd9c1
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-31 05:36:17 +0100

    style import

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,22 @@
-import { Link } from "react-router-dom"
 import logo from '../../assets/logo.png'
+import '../../styles/Header.css'
+import { Link } from "react-router-dom"
+
 
 function Header(){
     return(
-        <header>
-            <div>
-                <img className="logo" src={logo} alt="logo agence kasa" />
-            </div>
-            <nav>
-                <Link to="/">Acceuil</Link>
-                <Link to="/a-propos">A propos</Link>
-            </nav>
-
-        </header>
+        <>
+            <header>
+                <div>
+                    <img className="logo" src={logo} alt="logo agence kasa" />
+                </div>
+                <nav>
+                    <Link to="/">Acceuil</Link>
+                    <Link to="/a-propos">A propos</Link>
+                </nav>
+           </header>
+        </>
+        
     )
 }
 
