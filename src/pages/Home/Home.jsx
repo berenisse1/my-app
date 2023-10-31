1e48bb54641bcf54cd7998e7766fd6e59c70a8b9
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-31 05:46:35 +0100

    Props Banner Card update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,20 +2,29 @@ import Header from "../../components/Header/Header"
 import Banner from "../../components/Banner/Banner"
 import Card from "../../components/Card/Card"
 import Footer from "../../components/Footer/Footer"
+import bannerHome from "../../assets/bannerHome.png"
+import logements from "../../data/logements.json"
+import { Link } from "react-router-dom"
+
 
 function Home() {
     return(
-        <div>
-            <Header />
-            <h1>Home</h1>
+        <> 
+            <div>
+                <Header />
+            </div>
             <main>
-                <Banner/>
-                <div>
-                    <Card/>
+                <Banner image={bannerHome} texte= "Chez vous, partout et ailleurs"/>
+                <div className="card">
+                    {logements.map((logements)=>
+                    <Link to=  "logements/id" >
+                        <Card image={logements.cover} title={logements.title} key={logements.id} /> 
+                    </Link>
+                    )} 
                 </div>
             </main>
             <Footer/>
-        </div>
+        </>
     )
 }
 
