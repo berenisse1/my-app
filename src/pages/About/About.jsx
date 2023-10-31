34a00da7007b3a6676dac31acf1a92ff8bcb0965
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-31 05:42:26 +0100

    function creat

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,20 @@
+import Header from "../../components/Header/Header"
+import Banner from "../../components/Banner/Banner"
+import Footer from "../../components/Footer/Footer"
+import bannerAbout from "../../assets/bannerAbout.png"
+
+
 function About(){
     return(
+        <>
         <h1>About</h1>
+        <Header />
+            <main>
+                <Banner image={bannerAbout}/>
+            </main>
+        <Footer/>
+        </>
+        
     )
 }
 
