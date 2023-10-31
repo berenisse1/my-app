f4a78586c5503d3bba2be434641b48644528db12
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-10-31 06:25:42 +0100

    collapse

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+function Collapse (){
+    return(
+        <>
+        <div className= "collapse">
+            <div classeName = "collapse-Header">
+                <div classeName = "collapse-title"></div>
+                <img src="arow" alt="fléche d'accé à la description" />
+            </div>
+            <div classeName ="collapse-text"></div>
+        </div>
+        </>
+    
+    )
+         
+
+}
+
+ 
+ 
+ 
+ export default Collapse
