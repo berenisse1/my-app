0048317e81bf13cdaed6597e2fe7f0401cb1db3b
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-11-05 00:33:24 +0100

    Rate add

---
+++ src/components/Rate/Rate.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Rate () {
+    return (
+      <div className="Rate-container">
+        <img src="" alt="étoile" />
+        <img src="" alt="étoile vide" />  
+      </div>
+    );
+  }
+  
+  export default Rate;
