4befe35cb6fb42a815ab448bdf03b72fd216adce
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-11-05 00:25:41 +0100

    Tag add

---
+++ src/components/Tag.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Tag () {
+    return (
+      <div className="tag-container">
+        <p className="tag">{title}</p>
+      </div>
+    );
+  }
+  
+  export default Tag;
