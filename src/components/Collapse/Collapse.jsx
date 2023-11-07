b6acc15df99e175ce8d0197b1ccaf2ac11071afc
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-11-07 08:58:21 +0100

    event Open collapse update

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,21 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import'../../styles/Collapse.css';
 import arrow from '../../assets/arrow.png';
 
 
 
 function Collapse ({title, text}){
+
+    const [isOpen , setIsOpen] = useState(false);
+    let handleClick = () => {
+        isOpen = setIsOpen(true);
+          
+    }
+
     return(
         <>
         <div className="collapse">
-            <div className = "collapse-header">
+            <div className = "collapse-header" onClick={handleClick}>
                 <div className = "collapse-title">
                     {title}
                 </div>
                 <img src= {arrow} alt="flèche" />
             </div>
-            <div className ="collapse-text">
-                {text}
+            <div className ="collapse-text"> 
+               {isOpen? <p>{text}</p> : !isOpen }  
             </div>
         </div>
         </>
