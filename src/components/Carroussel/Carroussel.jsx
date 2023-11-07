4001cf76b020ce521545c2b7cf9d02391b6f69c6
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-11-07 04:07:59 +0100

    className update

--- src/components/Carroussel/Carroussel.jsx
+++ src/components/Carroussel/Carroussel.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import arrow from "../../assets/arrow.png"
+import arrow from "../../assets/arrow.png";
 
 function Caroussel ({image, text}) {
     return (
         <>
-            <div className="banner">
-                <img className="banner-image"  src={image} alt=""/>
-                <img className="arrow-left" src={arrow}  alt="arrow left previous"/>
-		        <img className="arrow-right" src= {arrow} alt="arrow right next"/>
+            <div className="slider">
+                <img className="slider-image"  src={image} alt=""/>
+                <img className="arrow arrow-left" src={arrow}  alt="arrow left previous"/>
+		        <img className="arrow arrow-right" src= {arrow} alt="arrow right next"/>
             </div>
-            <div className="banner-text">
+            <div className="slider-text">
                 <p>{text}</p>
             </div>
-            <div className='slide-number'>
+            <div className='slider-number'>
                 <p></p>
             </div>
         
