cdfee2c61db6672c3aca826291b5d0c2505000f9
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-11-13 01:58:33 +0100

    change function params and int compteur silde

--- src/components/Carroussel/Carroussel.jsx
+++ src/components/Carroussel/Carroussel.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
+import { useState } from "react";
 import arrow from "../../assets/arrow.png";
 
-function Caroussel ({image, text}) {
+function Caroussel ({slides, title, location}) {
+    
+    let [compteur , setCompteur] = useState(0);
+
+
     return (
         <>
             <div className="slider">
-                <img className="slider-image"  src={image} alt=""/>
-                <img className="arrow arrow-left" src={arrow}  alt="arrow left previous"/>
-		        <img className="arrow arrow-right" src= {arrow} alt="arrow right next"/>
+                <div>
+                    <img className="slider-image"  src={slides[compteur]} alt=""/>
+                </div>
+                <div>
+                    <button>
+                        <img className="arrow arrow-left" src={arrow}  alt="arrow left previous"/>
+                    </button> 
+		            <button>
+                        <img className="arrow arrow-right" src= {arrow} alt="arrow right next"/>
+                    </button> 
+                </div>  
             </div>
             <div className="slider-text">
-                <p>{text}</p>
+                <p>{title}</p>
+                <p>{location}</p>
             </div>
             <div className='slider-number'>
-                <p></p>
+                <p>{compteur+1}/{slides.length}</p>
             </div>
         
            
         </>
     
     );
-  }
+}
   
 export default Caroussel;
-
 
 
   
