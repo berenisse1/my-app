9fbddea031ddc83cd84073da714b504ac524f557
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-11-14 09:18:13 +0100

    carrousel btn delete style update

--- src/components/Carroussel/Carroussel.jsx
+++ src/components/Carroussel/Carroussel.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from "react";
 import arrowRight from "../../assets/arrowRight.png";
 import arrowLeft from "../../assets/arrowLeft.png";
+import "../../styles/Carroussel.css";
 
 function Caroussel ({slides, title, location}) {
     
@@ -17,29 +18,23 @@ function Caroussel ({slides, title, location}) {
     return (
         <>
             <div className="slider">
-                <div>
-                    <img className="slider-image"  src={slides[compteur]} alt=""/>
+                <div className="slider-image" >
+                    <img src={slides[compteur]} alt=""/>
                 </div>
                 <div>
-                    <button>
-                        <img className="arrow arrow-left" src={arrowLeft} alt="arrow left previous"onClick={previous}/>
-                        {(compteur < 0) && setCompteur(slides.length-1)}
-                    </button> 
-		            <button>
-                        <img className="arrow arrow-right" src= {arrowRight} alt="arrow right next"onClick={next}/>
-                        {(compteur >= slides.length) && setCompteur(0)}
-                    </button> 
+                    <img className="arrow arrow-left" src={arrowLeft} alt="arrow left previous"onClick={previous}/>
+                    {(compteur < 0) && setCompteur(slides.length-1)}
+                    <img  className="arrow arrow-right" src= {arrowRight} alt="arrow right next"onClick={next}/>
+                    {(compteur >= slides.length) && setCompteur(0)}
+                    <div className='slider-number' >
+                        {compteur+1}/{slides.length}
+                    </div>
                 </div>  
             </div>
             <div className="slider-text">
                 <p>{title}</p>
                 <p>{location}</p>
-            </div>
-            <div className='slider-number'>
-                <p>{compteur+1}/{slides.length}</p>
-            </div>
-        
-           
+            </div>  
         </>
     
     );
