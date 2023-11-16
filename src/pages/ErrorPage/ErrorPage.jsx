1f04a4e3d79cb821c3fd8fcbd3864dddda1a4d9f
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-11-16 20:50:15 +0100

    error component remove and add into errorPage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import Error from "../../components/Error/Error";
+//import Error from "../../components/Error/Error";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import { Link } from "react-router-dom";
 
 
 function ErrorPage() {
     return(
         <div>
             <Header />
-            <h1>ErrorPage</h1>
             <main>
-                <Error/>
+                <div>
+                    <p className="error-code">404</p>
+                    <p className="error-text">
+                        Oups! La page que vous demandez n'existe pas
+                    </p>
+                    <nav>
+                        <Link to="/">Retournez sur la page d'acceuil</Link>
+                    </nav>
+                </div>
             </main>
             <Footer/>
         </div>
