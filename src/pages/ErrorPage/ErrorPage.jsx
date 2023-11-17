475e96a88d5ec1be413c2fc3bca5cdb87edda136
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-11-17 09:50:07 +0100

    errorPage style update

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-//import Error from "../../components/Error/Error";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import { Link } from "react-router-dom";
+import "../../styles/ErrorPage.css";
 
 
 function ErrorPage() {
@@ -10,13 +10,15 @@ function ErrorPage() {
         <div>
             <Header />
             <main>
-                <div>
-                    <p className="error-code">404</p>
+                <div className="error-container">
+                    <p className="error-code">
+                        404
+                    </p>
                     <p className="error-text">
                         Oups! La page que vous demandez n'existe pas
                     </p>
                     <nav>
-                        <Link to="/">Retournez sur la page d'acceuil</Link>
+                        <Link className="link-retour-home" to="/">Retournez sur la page d'acceuil</Link>
                     </nav>
                 </div>
             </main>
